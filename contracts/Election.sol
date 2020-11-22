d88d9ec042f90586f870223ccffef24eeb9a877a
ksatyarth2 <erkumar@protonmail.ch>
2020-11-23 02:56:53 +0530

    smoke test

---
+++ contracts/Election.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.4.2 <0.8.0;
+
+contract Election{
+    
+    string public candidate;
+
+    //constructor
+
+    constructor () public{
+        candidate = "Candidate 1";
+    }
+}
